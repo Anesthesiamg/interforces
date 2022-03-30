0c6273ab9dde5b2c84bcf8343fc903a8be5c8018
samuraixxx <52816802+xxSamuraiXxx@users.noreply.github.com>
2022-03-30 21:22:52 +0300

    Update and rename testt to testt.php

---
+++ testt.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Arifcan Ekmekci</title>			<!--SİTE BAŞLIĞINIZ-->
+
+<style type="text/css">
+/* Eric Myer's Global Reset http://meyerweb.com/eric/tools/css/reset/index.html v1.0 | 20080212 */
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, font, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td {
+	margin: 0;
+	padding: 0;
+	border: 0;
+	outline: 0;
+	font-size: 100%;
+	vertical-align: baseline;
+	background: transparent;
+}
+
+#true-random-integer-generator {
+	font-family: verdana, sans;
+	font-size: 35pt;
+	background: #FFFFFF;
+	padding: 5px;
+	margin: 0px;
+	width: 1000px;
+	color: #979797;
+	
+		border: 1px solid #ff8b8d;}
+
+#true-random-integer-generator-title {
+	text-align: center;
+	padding: 1px;
+	display: block;
+	background: #ff8b8d;
+	color: #000000;
+	margin: 100px;
+	margin-bottom: 10px;
+}
+
+
+#true-random-integer-generator-min-span {
+	display: block;
+	margin-bottom: 5px;
+}
+
+#true-random-integer-generator-min-span label {
+	color: #000000;
+}
+
+#true-random-integer-generator-min-span input {
+	width: 90px;
+	height: 40px;
+	margin-left: 25px;
+}
+
+#true-random-integer-generator-max-span {
+	display: block;
+	margin-bottom: 5px;
+}
+
+#true-random-integer-generator-max-span label {
+	color: #000000;
+}
+
+#true-random-integer-generator-max-span input {
+	width: 120px;
+	height: 40px;
+	margin-left: 25px;
+}
+
+#true-random-integer-generator-button {
+	color: #224059;
+    	background-color: #b5c9e2;
+    	border-top: 10px #cce3ff solid;
+    	border-left: 10px #cce3ff solid;
+    	border-bottom: 10px #31557f solid;
+    	border-right: 10px #31557f solid;
+    	font:bold 12px Arial, Helvetica, sans-serif;
+}
+
+#true-random-integer-generator-result {
+	display: block;
+	padding: 2px;
+	margin-bottom: 50px;
+	color: #000000;
+	background:  #ff8b8d;
+	font-size: 15pt;
+}
+
+#true-random-integer-generator-credits {
+	display: block;
+	font-size: 15pt;
+	text-align: center;
+	padding: 5px;
+}
+
+#true-random-integer-generator-credits a {
+	color: #ff8b8d;
+}
+
+#true-random-integer-generator-credits a:hover {
+	color: #ff8b8d;
+}
+</style>
+
+<script type="text/javascript">
+    if ("https:" == document.location.protocol) {
+      document.write(unescape("%3Cscript src='https://www.google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+    } else {
+      document.write(unescape("%3Cscript src='http://www.google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+    }
+  </script>
+ <font size="8"> <script type="text/javascript">
+    var rdoIframeTracker = _gat._getTracker("UA-6153345-1");
+    rdoIframeTracker._setDomainName("none");
+    rdoIframeTracker._setAllowLinker(true);
+  <button onclick="sayiUret();">Birini Sec</button> </font>
+   <script>
+    function sayiUret() {
+      document.getElementById("sayi").innerHTML = randomInt(1, 331);
+    }
+
+    function randomInt(min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+  </script>
+  
+
+<script language="javascript" type="text/javascript" src="iframe.js"></script>
+
+</head>
+
+<body>
+
+<center><div id="true-random-integer-generator">
+	<span id="true-random-integer-generator-title">True Random Number Generator</span>
+	<span id="true-random-integer-generator-min-span">
+		<label for="true-random-integer-generator-min">Min:</label>
+		<input type="number" name="true-random-integer-generator-min" id="true-random-integer-generator-min" maxlength="9" value="1" onkeypress="return integerJsInputControl(event);" />
+	</span>
+	<span id="true-random-integer-generator-max-span">
+		<label for="true-random-integer-generator-max">Max:</label>
+		<input type="number" name="true-random-integer-generator-max" id="true-random-integer-generator-max" maxlength="9" value="1000" onkeypress="return integerJsInputControl(event);" />
+	</span>
+	<span id="true-random-integer-generator-max-button-span">
+  <button onclick="sayiUret();">Birini Sec</button>
+	</span>
+  <center><div id="sonuc"> Result; <span id="sayi"></span></div></center>
+	<span id="true-random-integer-generator-result"></span>
+</div></center>
+
+</body>
+</html>
+
+
+
+
+
+
+	<span id="true-random-integer-generator-credits">Tüm Hakları Saklıdır <a href="https://www.instagram.com/arifcanekmekci" target="_blank">ArifcanEkmekci</a></span>
+
+
+
+
+<!DOCTYPE html>
+<html lang="tr">
+
+<head>
+  <meta charset="UTF-8">
+  <title>ArifcanEkmekci Çekiliş</title>
+</head>
+
+<body>
+  <script>
+    function sayiUret() {
+      document.getElementById("sayi").innerHTML = randomInt(1, 1);		<!--HANGİ SAYIYA ÇIKARMAK İSTİYORSANIZ BU ŞEKİLDE YAZIN ÖRNEGİN 1 İÇİN 1, 1 GİBİ-->
+    }
+
+    function randomInt(min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+  </script>
+</body>
+
+</html>
+
+
+
+<!-- arifcanekmekci.com/toplist - sayac kodu -->
+<script type="text/javascript" src="http://arifcanekmekci.com/toplist/counter.php"></script>
+<!-- arifcanekmekci.com/toplist - sayac kodu -->
